6e26980fd86753fc443889573aa12775857e85f2
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-05-13 17:26:20 +0300

    categories widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Pages/HomePages.dart';
+import 'package:flutter_application_1/Pages/HomePage.dart';
 
 void main() {
   runApp(MyApp());
